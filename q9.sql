73878f5508f988ddc922f9e64778d79b37f22681
Xinyue124 <113944200+Xinyue124@users.noreply.github.com>
2023-10-16 13:02:12 -0500

    Update q9.sql

--- q9.sql
+++ q9.sql
@@ -1,1 +1,5 @@
-SELECT departments.department_name FROM departments, employees WHERE departments.department_id = employees.department_id GROUP BY departments.department_id HAVING employees.salary = (SELECT MAX(employees.salary) FROM employees);
+SELECT d.department_name
+FROM departments d
+JOIN employees e ON d.department_id = e.department_id
+WHERE e.salary = (SELECT MAX(salary) FROM employees)
+LIMIT 1;
