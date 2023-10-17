e5166c9881f893257e2198db4a2236b4a13c5b96
Xinyue124 <113944200+Xinyue124@users.noreply.github.com>
2023-10-16 20:19:42 -0500

    Update q7.sql

--- q7.sql
+++ q7.sql
@@ -1,10 +1,8 @@
-SELECT e1.first_name || ' ' || e1.last_name AS employee_name_1,
-       e2.first_name || ' ' || e2.last_name AS employee_name_2
-FROM employees e1
-JOIN employees e2 ON e1.department_id = e2.department_id
-                  AND e1.manager_id = e2.manager_id
-                  AND e1.salary >= e2.salary
-WHERE e1.employee_id <> e2.employee_id
-      AND e1.salary > 10000 
-      AND e2.salary > 10000
-ORDER BY e1.first_name, e1.last_name, e2.first_name, e2.last_name;
+SELECT e1.first_name, e2.first_name
+FROM employees e1, employees e2 
+WHERE e1.department_id = e2.department_id
+AND e1.salary >= e2.salary
+AND e1.salary > 10000 
+AND e2.salary > 10000
+AND e1.manager_id = e2.manager_id
+AND e1.employee_id != e2.employee_id
